e43cc7bf27c00f86602f2c9e231fcf633e27ad33
YANDRAPRAGADA NARASIMHA RAO <99276701+Narasimharao2003@users.noreply.github.com>
2024-06-29 13:54:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+<!-- Main content -->
+<div class="container-fluid" style="margin:50px;">
+    <div class="row container" style="text-align: justify;">
+        <div class="col-md-8 mx-auto content-left">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+            Blood bank management involves the efficient coordination of processes related to blood donation, collection, testing, storage, and distribution. It ensures that donated blood is safe, readily available, and appropriately utilized for transfusions to patients in need. Through advanced technology and streamlined procedures, blood bank management systems enhance the effectiveness of blood banking operations. 
+            Our Blood Bank Management System revolutionizes the blood donation process by providing a user-friendly platform for donor engagement, secure blood collection, real-time inventory control, automated testing and processing, efficient distribution to healthcare facilities, and comprehensive reporting. Donors can easily register, schedule appointments, and receive updates on donation drives. With strict protocols for screening and eligibility checks, donor and recipient safety is ensured during collection. Hospitals can request products swiftly, ensuring timely delivery to meet patient needs. Join us in saving lives and improving healthcare
+            </p>
+        </div>
+        <div class="col-md-3 content-right">
+            <img class="img-fluid" src="../BBMS/images/image1.jpg" height="auto" width="70%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>Why Do We Need Blood Bank Management? </h4>
+            <p>
+               Blood bank management is crucial for several reasons. Firstly, it ensures the safety of donated blood by implementing rigorous screening and testing procedures, thus safeguarding both donors and recipients from infectious diseases. Secondly, effective management practices help maintain an adequate supply of blood products by overseeing donor recruitment and inventory levels, preventing shortages and ensuring a steady flow of blood to meet patient needs. Thirdly, by facilitating efficient distribution and inventory control, blood bank management systems enable hospitals to access blood products promptly, ensuring timely transfusions for patients undergoing surgeries, treatments, or emergencies. Lastly, by minimizing wastage and optimizing resource allocation, blood bank management maximizes the impact of blood donations and enhances healthcare delivery overall.
+            </p>
+            <p>
+            Moreover, blood bank management plays a crucial role in disaster preparedness and emergency response. During natural disasters, accidents, or large-scale emergencies, the demand for blood and blood products can spike significantly. Properly managed blood banks can quickly mobilize resources, coordinate donation drives, and allocate blood supplies where they are needed most, saving lives in critical situations. This rapid response capability underscores the vital role of organized blood bank systems in supporting healthcare systems during crises and ensuring that lifesaving blood products are readily available when communities face unforeseen challenges.
+           </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-bottom: 0%;">
+    <div class="row">
+        <div class="col-12">
+            <h4 style="margin-left: 120px;">Some Questions</h4>
+        </div>
+        <div class="col-md-3">
+            <div class="card mb-1 h-100">
+                <h4 class="card-header">Who can donate?</h4>
+                <div class="card-body">
+                    <p class="card-text">You need to be 18-65 years old, weigh 45kg or more, and be fit and healthy.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mb-1 h-100">
+                <h4 class="card-header">Hemoglobin level for donation?</h4>
+                <div class="card-body">
+                    <p class="card-text">Men need hemoglobin of 13.0-13.5 g/dL, and women need 12.0-12.5 g/dL to donate blood safely.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mb-1 h-100">
+                <h4 class="card-header">How often can I donate?</h4>
+                <div class="card-body">
+                    <p class="card-text">Male donors can donate again after 90 days, and female donors can donate again after 120 days.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mb-1 h-100">
+                <h4 class="card-header">How long does it take to donate?</h4>
+                <div class="card-body">
+                    <p class="card-text">Blood donation, including pre-donation checks, usually takes 15-30 minutes, ensuring a quick and efficient process for contributing to healthcare.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+>
+
+    
+</div>
+
+
+</div>
+
+<div style="padding-left: 120px; padding-top:20px">
+<h4>Blood Donation Center Details</h4>
+<a target="_blank" href="https://indianredcross.org/ircs/ircsbloodbanks">Blood Donation Centers</a>
+</div>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-danger footer">
+        CareBlood Donor Society Organization
+        </div>
+    </div>
+</div>
+</body>
+</html>
